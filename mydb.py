9bd462ef901be1cdab7b25674b0d9339d2c22007
Lucachak <lucas.lucachak9@gmail.com>
2024-06-30 17:41:54 +0200

    Test Crud

---
+++ mydb.py
@@ -0,0 +1,14 @@
+import mysql.connector
+
+
+dataBase = mysql.connector.connect(
+    host='localhost',
+    user="root",
+    passwd='lucas123'
+)
+
+cursorObject = dataBase.cursor()
+
+cursorObject.execute("CREATE DATABASE myDB")
+
+print("all Done!1")
