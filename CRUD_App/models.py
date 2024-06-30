e0124f6c5c3e848c859365a39e05d47f5a1d5f8b
Lucachak <lucas.lucachak9@gmail.com>
2024-06-30 23:52:25 +0200

    new file:   CRUD_App/__pycache__/forms.cpython-312.pyc 	modified:   CRUD_App/__pycache__/models.cpython-312.pyc 	modified:   CRUD_App/__pycache__/views.cpython-312.pyc 	modified:   CRUD_App/forms.py 	modified:   CRUD_App/models.py 	modified:   CRUD_App/templates/register.html 	modified:   CRUD_App/views.py

--- CRUD_App/models.py
+++ CRUD_App/models.py
@@ -1,1 +1,15 @@
 from django.db import models
+
+
+class Record(models.Model):
+    create_at = models.DateTimeField(auto_now_add=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    zip_code = models.CharField(max_length=50)
+
+    def __str__(self):
+        return(f"{self.first_name} {self.last_name}")
