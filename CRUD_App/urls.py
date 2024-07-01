005956f574aef8ded838754a48be58f5142dae26
Lucachak <lucas.lucachak9@gmail.com>
2024-07-01 14:55:31 +0200

    Register+ Add+ List Created!

--- CRUD_App/urls.py
+++ CRUD_App/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path("login/", views.login_user, name='login'),
     path("logout/", views.logout_user, name='logout'),
     path("register/", views.register_user, name='register_user'),
-    path("records/<int:pk>", views.customer_record, name='record')
+    path("records/<int:pk>", views.customer_record, name='record'),
+    path("delete_records/<int:pk>", views.delete_record, name='delete_record'),
+    path("add_records/", views.add_records, name='add_records'),
 ]
--- CRUD_App/views.py
+++ CRUD_App/views.py
@@ -1,31 +1,29 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .forms import SignUpForm
+from .forms import SignUpForm,AddRecordForm
 from .models import Record 
 
 
 
 
 def home(request):
-
-    records = Record.objects.all()
-
-
-    if request.method == "POST":
-        username = request.POST['username']
-        password = request.POST['password']
-
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            messages.success(request, "You have been logged in")
-            return redirect('home')
-        else:
-            messages.success(request, "There was an error! ")
-            return redirect('home')
-    else:
-        return render(request, "home.html", {'records': records})
+	records = Record.objects.all()
+	# Check to see if logging in
+	if request.method == 'POST':
+		username = request.POST['username']
+		password = request.POST['password']
+		# Authenticate
+		user = authenticate(request, username=username, password=password)
+		if user is not None:
+			login(request, user)
+			messages.success(request, "You Have Been Logged In!")
+			return redirect('home')
+		else:
+			messages.success(request, "There Was An Error Logging In, Please Try Again...")
+			return redirect('home')
+	else:
+		return render(request, 'home.html', {'records':records})
     
 
 
@@ -62,3 +60,27 @@ def customer_record(request, pk):
     else:
         messages.success(request,"You Can't Do That, You're Not Logged In!")
         return redirect('home')
+
+def delete_record(request,pk ):
+    if request.user.is_authenticated:
+        delete_it = Record.objects.get(id=pk)
+        delete_it.delete()
+        messages.success(request,"Record Deleted!")
+        return redirect('home')
+    else:
+        messages.success(request,"You must be logged!")
+        return redirect('home')
+
+
+def add_records(request):
+	form = AddRecordForm(request.POST or None)
+	if request.user.is_authenticated:
+		if request.method == "POST":
+			if form.is_valid():
+				add_record = form.save()
+				messages.success(request, "Record Added...")
+				return redirect('home')
+		return render(request, 'add_records.html', {'form':form})
+	else:
+		messages.success(request, "You Must Be Logged In...")
+		return redirect('home')
