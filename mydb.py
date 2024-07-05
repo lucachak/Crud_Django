9589ef9986bcd813ee21e4105a9dec2718b75718
Lucachak <lucas.lucachak9@gmail.com>
2024-07-05 20:41:24 +0200

    update

--- mydb.py
+++ mydb.py
@@ -3,12 +3,12 @@ import mysql.connector
 
 dataBase = mysql.connector.connect(
     host='localhost',
-    user="****",
-    passwd='*******'
+    user="root",
+    passwd='lucas123'
 )
 
 cursorObject = dataBase.cursor()
 
-cursorObject.execute("CREATE DATABASE myDB")
+cursorObject.execute("CREATE DATABASE expense")
 
 print("all Done!1")
