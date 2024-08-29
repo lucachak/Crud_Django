2d30eca1e52ab5e070d3bfb2cdba3ad4770dfc4e
lucachak <75806171+lucachak@users.noreply.github.com>
2024-08-29 20:57:56 +0200

    Update views.py

--- CRUD_App/views.py
+++ CRUD_App/views.py
@@ -46,7 +46,19 @@ def register_user(request):
 
 
 def login_user(request):
-    pass
+        if request.method =='POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, ("You've beem logged in"))
+            return redirect('home')
+        else:
+            messages.success(request,("There was an error!"))
+            return redirect('login')
+    else:
+        return render(request, 'login.html',{})
     
 def logout_user(request):
     logout(request)
